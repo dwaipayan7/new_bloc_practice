97a0cac980c687d1b919d89a0da1bf241782fc53
Dwaipayan <biswastatay73@gmail.com>
2024-09-10 04:36:30 +0530

    Update the Home Bloc

---
+++ lib/features/home/bloc/home_state.dart
@@ -0,0 +1,24 @@
+part of 'home_bloc.dart';
+
+@immutable
+sealed class HomeState {}
+
+abstract class HomeActionState extends HomeState{}
+
+final class HomeInitial extends HomeState {}
+
+class HomeLoadingState extends HomeState{}
+
+class HomeLoadedSuccessState extends HomeState{
+  final List<ProductDataModel> product;
+
+  HomeLoadedSuccessState({required this.product});
+}
+
+class HomeErrorState extends HomeState{}
+
+class HomeNavigateToWishlistPageActionState extends HomeActionState{}
+
+class HomeNavigateToCartPageActionState extends HomeActionState{}
+
+
